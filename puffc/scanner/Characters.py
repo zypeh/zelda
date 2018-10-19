3c7222d74f7562f3a8a8ad817a7c7d43bd10f9d8
Jeff <goodboy96@live.com.my>
2018-10-19 16:39:49 +0800

    Add characters enum

--- puffc/scanner/Characters.py
+++ puffc/scanner/Characters.py
@@ -1,2 +1,137 @@
-def hello():
-    print("hello")
+from enum import Enum
+
+
+class Character(Enum):
+    NULLCHARACTER = 0
+    MAXASCIICHARACTER = 0x7f
+
+    LINEFEED = 0x0a  # \n
+    CARRIAGERETURN = 0x0d  # \r
+    LINESEPARATOR = 0x2028
+    PARAGRAPHSEPARATOR = 0x2029
+    NEXTLINE = 0x0085
+
+    # Unicode 3.0 space characters
+    SPACE = 0x0020  # " "
+    NONBREAKINGSPACE = 0x00a0
+    ENQUAD = 0x2000
+    EMQUAD = 0x2001
+    ENSPACE = 0x2002
+    EMSPACE = 0x2003
+    THREEPEREMSPACE = 0x2004
+    FOURPEREMSPACE = 0x2005
+    SIXPEREMSPACE = 0x2006
+    FIGURESPACE = 0x2007
+    PUNCTUATIONSPACE = 0x2008
+    THINSPACE = 0x2009
+    HAIRSPACE = 0x200a
+    ZEROWIDTHSPACE = 0x200b
+    NARROWNOBREAKSPACE = 0x202f
+    IDEOGRAPHICSPACE = 0x3000
+    MATHEMATICALSPACE = 0x205f
+    OGHAM = 0x1680
+
+    _ = 0x5f
+    $ = 0x24
+
+    _0 = 0x30
+    _1 = 0x31
+    _2 = 0x32
+    _3 = 0x33
+    _4 = 0x34
+    _5 = 0x35
+    _6 = 0x36
+    _7 = 0x37
+    _8 = 0x38
+    _9 = 0x39
+
+    A = 0x61
+    B = 0x62
+    C = 0x63
+    D = 0x64
+    E = 0x65
+    F = 0x66
+    G = 0x67
+    H = 0x68
+    I = 0x69
+    J = 0x6a
+    K = 0x6b
+    L = 0x6c
+    M = 0x6d
+    N = 0x6e
+    O = 0x6f
+    P = 0x70
+    Q = 0x71
+    R = 0x72
+    S = 0x73
+    T = 0x74
+    U = 0x75
+    V = 0x76
+    W = 0x77
+    X = 0x78
+    Y = 0x79
+    Z = 0x7a
+
+    A = 0x41
+    B = 0x42
+    C = 0x43
+    D = 0x44
+    E = 0x45
+    F = 0x46
+    G = 0x47
+    H = 0x48
+    I = 0x49
+    J = 0x4a
+    K = 0x4b
+    L = 0x4c
+    M = 0x4d
+    N = 0x4e
+    O = 0x4f
+    P = 0x50
+    Q = 0x51
+    R = 0x52
+    S = 0x53
+    T = 0x54
+    U = 0x55
+    V = 0x56
+    W = 0x57
+    X = 0x58
+    Y = 0x59
+    Z = 0x5a
+
+    AMPERSAND = 0x26  # &
+    ASTERISK = 0x2a  # *
+    AT = 0x40  # @
+    BACKSLASH = 0x5c  # \
+    BACKTICK = 0x60  # `
+    BAR = 0x7c  # |
+    CARET = 0x5e  # ^
+    CLOSEBRACE = 0x7d  # }
+    CLOSEBRACKET = 0x5d  # ]
+    CLOSEPAREN = 0x29  # )
+    COLON = 0x3a  # :
+    COMMA = 0x2c
+    DOT = 0x2e  # .
+    DOUBLEQUOTE = 0x22  # "
+    EQUALS = 0x3d  # =
+    EXCLAMATION = 0x21  # !
+    GREATERTHAN = 0x3e  # >
+    HASH = 0x23
+    LESSTHAN = 0x3c  # <
+    MINUS = 0x2d  # -
+    OPENBRACE = 0x7b  # {
+    OPENBRACKET = 0x5b  # [
+    OPENPAREN = 0x28  # (
+    PERCENT = 0x25  # %
+    PLUS = 0x2b  # +
+    QUESTION = 0x3f  # ?
+    SEMICOLON = 0x3b  # ;
+    SINGLEQUOTE = 0x27  # '
+    SLASH = 0x2f  # /
+    TILDE = 0x7e  # ~
+
+    BACKSPACE = 0x08  # \b
+    FORMFEED = 0x0c  # \f
+    BYTEORDERMARK = 0xfeff
+    TAB = 0x09  # \t
+    VERTICALTAB = 0x0b  # \v
